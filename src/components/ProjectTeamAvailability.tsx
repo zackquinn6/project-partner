73aa11b1596c34ea58290c09e281fcff0ad1d41a
ZackQuinn6 <zackquinn6@gmail.com>
2025-11-21 13:46:57 -0500

    Fix: Remove unused useEffect import from ProjectTeamAvailability

--- src/components/ProjectTeamAvailability.tsx
+++ src/components/ProjectTeamAvailability.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
