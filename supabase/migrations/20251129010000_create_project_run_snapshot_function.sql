aa377288eb42b720bdd32b80020b479939452d85
ZackQuinn6 <zackquinn6@gmail.com>
2025-11-28 23:14:22 -0500

    Fix create_project_run_snapshot: cast source columns to UUID in inner query to resolve polymorphic type error in outer CASE statements

--- supabase/migrations/20251129010000_create_project_run_snapshot_function.sql
+++ supabase/migrations/20251129010000_create_project_run_snapshot_function.sql
@@ -109,11 +109,11 @@ BEGIN
       phase_project_id::UUID,
       CASE 
         WHEN phase_project_id = '00000000-0000-0000-0000-000000000001'::UUID THEN NULL::UUID
-        ELSE COALESCE(phase_source_project_id::UUID, NULL::UUID)
+        ELSE phase_source_project_id
       END AS phase_source_project_id,
       CASE 
         WHEN phase_project_id = '00000000-0000-0000-0000-000000000001'::UUID THEN NULL::UUID
-        ELSE COALESCE(phase_source_phase_id::UUID, NULL::UUID)
+        ELSE phase_source_phase_id
       END AS phase_source_phase_id,
       sort_order::INTEGER
     FROM (
@@ -125,8 +125,8 @@ BEGIN
         pp.position_rule AS phase_position_rule,
         pp.position_value AS phase_position_value,
         pp.project_id AS phase_project_id,
-        pp.source_project_id AS phase_source_project_id,
-        pp.source_phase_id AS phase_source_phase_id,
+        pp.source_project_id::UUID AS phase_source_project_id,
+        pp.source_phase_id::UUID AS phase_source_phase_id,
         CAST(
           CASE 
             WHEN pp.position_rule = 'first' THEN 1
